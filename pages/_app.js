70d2b64983edbde402b568adee724d68b7bb2f11
White Dog <dlrlgus1204@naver.com>
2022-04-13 14:36:36 +0900

    concept work

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,9 @@ import NavBar from "../components/NavBar";
 export default function App({ Component, pageProps }) {
     const [cnt, setCnt] = useState(0);
     return <>
-        <NavBar />
+        {/* <NavBar /> */}
         <Component {...pageProps} />
-        <button onClick={() => setCnt((prev) => prev + 1)}>클릭</button>
-        <div>{cnt}</div>
+        {/* <button onClick={() => setCnt((prev) => prev + 1)}>클릭</button>
+        <div>{cnt}</div> */}
     </>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,86 @@
 export default function Home() {
     return (
         <>
-            <div>hi</div>
+            <div className="container">
+                <div className="topContainer">
+                    <div className="icon"></div>
+                    <input className="search"></input>
+                </div>
+                <div className="midContainer">
+                    <div className="leftMid">
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="rightMid">
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+
+            <style jsx>{`
+                .container {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .topContainer {
+                    display: flex;
+                    align-items: center;
+                }
+                .midContainer {
+                    width: 1200px;
+                    height: 700px;
+                    background: white;
+                    border: 1px black solid;
+                    display:flex;
+                }
+                .leftMid {
+                    width:50%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .leftMid > div {
+                    width: 90%;
+                    height: 50%;
+                    margin: 10px;
+                    border-radius: 20px;
+                    background: pink;
+                }
+                .rightMid {
+                    width: 50%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .rightMid > div {
+                    width: 90%;
+                    height: 100%;
+                    margin: 10px;
+                    background: pink;
+                    border-radius: 20px;
+                }
+                .icon {
+                    width: 80px;
+                    height: 80px;
+                    background-color: white;
+                    border: 1px black solid;
+                    border-radius: 50%;
+                    margin: 10px;
+                }
+                .search {
+                    background-color: white;
+                    border: 1px black solid;
+                    width: 400px;
+                    height: 20px;
+                    margin: 10px;
+                    border-radius: 20px;
+                    padding: 10px;
+                }
+            `}</style>
         </>
     );
 }
